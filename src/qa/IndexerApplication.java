aa3fc448e48c4bb1d96c6bb0ba64a04cbe57b75d
Ha Duy Trung <haduytrung@gmail.com>
2013-04-07 12:44:17 +0800

    Clear indexed data if force option is used

--- src/qa/IndexerApplication.java
+++ src/qa/IndexerApplication.java
@@ -1,6 +1,7 @@
 package qa;
 
 import java.util.List;
+import java.io.File;
 
 import qa.Settings;
 import qa.helper.ApplicationHelper;
@@ -14,8 +15,16 @@ public class IndexerApplication {
     public static void main(String[] args) {
         System.err.close();
         DocumentIndexer indexer = new LuceneIndexer();
-        if (args.length > 0 && args[0].equals("-f") || !indexer.hasIndexData(Settings.get("INDEX_PATH"))) {
+        boolean forceIndex = args.length > 0 && args[0].equals("-f");
+        if (forceIndex || !indexer.hasIndexData(Settings.get("INDEX_PATH"))) {
             try {
+                File dir = new File(Settings.get("INDEX_PATH"));
+                if (dir.isDirectory()) {
+                    for (File f : dir.listFiles()) {
+                        f.delete();
+                    }
+                }
+
                 indexer.indexDocuments(Settings.get("DOCUMENT_PATH"));    
             } catch (Exception e) {
                 ApplicationHelper.printError(e.getMessage());
@@ -27,12 +36,14 @@ public class IndexerApplication {
             System.out.println("To query, run 'java qa.IndexerApplication \"your query\"'.");
         }
 
-        DocumentRetrieverImpl retriever = new DocumentRetrieverImpl();
-        for (int i = 0; i < args.length; i++) {
-            String query = args[i];
-            System.out.println("Query: " + query);
-            List<Document> results = retriever.getDocuments(query);
-            print(results);
+        if (!forceIndex) {
+            DocumentRetrieverImpl retriever = new DocumentRetrieverImpl();
+            for (int i = 0; i < args.length; i++) {
+                String query = args[i];
+                System.out.println("Query: " + query);
+                List<Document> results = retriever.getDocuments(query);
+                print(results);
+            }
         }
     }
 
